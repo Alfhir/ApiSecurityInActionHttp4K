cc1c532337937e68f83856d83fa52b4988c23774
Oliver <oliver.eisenbarth@gmail.com>
2023-10-28 00:11:48 +0200

    Fix bad sql

--- src/main/resources/db/migration/V1.1__Create_API_User.sql
+++ src/main/resources/db/migration/V1.1__Create_API_User.sql
@@ -1,3 +1,3 @@
 CREATE USER dungeon_api_user PASSWORD 'password';
-GRANT SELECT, INSERT ON dungeon, room, monster, TO dungeon_api_user;
+GRANT SELECT, INSERT ON dungeon, room, monster TO dungeon_api_user;
 GRANT DELETE ON monster TO dungeon_api_user;
