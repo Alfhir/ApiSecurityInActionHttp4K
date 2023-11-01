d23645e575bc0f2184aa5f55d9d6ba2bd16c37ed
Oliver <oliver.eisenbarth@gmail.com>
2023-11-01 14:01:49 +0100

    Add tracer bullet module, reorder dependencies
    I seem to be stuck exploring Http4K's awesomeness, so I might as well go the full mile.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,15 @@ flyway {
 
 dependencies {
     implementation(platform("org.http4k:http4k-bom:$http4kVersion"))
-    // implementation("org.http4k:http4k-cloudnative")
-    implementation("org.http4k:http4k-contract")
     implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-contract")
     implementation("org.http4k:http4k-format-jackson")
+    implementation("org.http4k:http4k-testing-tracerbullet")
 
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
-    testImplementation("org.http4k:http4k-testing-hamkrest")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
 
     implementation("org.postgresql:postgresql:42.6.0")
     implementation("org.flywaydb:flyway-core:9.22.3")
